2b2578e0b78d82db6e923d0b120aea3716cc7c98
ShadePrima <aleksandrag1984@gmail.com>
2023-08-24 14:17:02 +0300

    add testimonial section

--- components/testimonial/Testimonial.tsx
+++ components/testimonial/Testimonial.tsx
@@ -2,6 +2,37 @@ import React from 'react'
 import styles from './Testimonial.module.scss'
 import TestimonialCard from './TestimonialCard'
 
+import image1 from '../../assets/images/testimonial1.png'
+import image2 from '../../assets/images/testimonial2.png'
+import image3 from '../../assets/images/testimonial3.png'
+
+const testimonialCardList = [
+  {
+    id: 1,
+    image: image1,
+    name: 'Eleanor Pena',
+    job: 'UI/UX Designer',
+    description:
+      'Customer testimonials are more effective than paid marketing copy as they take the spotlight away from the seller to shine it on the customers. ',
+  },
+  {
+    id: 2,
+    image: image2,
+    name: 'Theresa Webb',
+    job: 'Vlogger',
+    description:
+      'In promotion and advertising, a testimonial or show consists of a person"s written statement extolling the virtue of a product.',
+  },
+  {
+    id: 3,
+    image: image3,
+    name: 'Annette Black',
+    job: 'Doctor',
+    description:
+      'Testimonials work because they aren"t strong sales pitches, they come across in an unbiased voice and establish trust',
+  },
+]
+
 const Testimonial = () => {
   return (
     <div className={styles.main}>
@@ -12,7 +43,16 @@ const Testimonial = () => {
         </h1>
 
         <div className={styles.cardContainer}>
-          <TestimonialCard />
+          {testimonialCardList.map((item) => (
+            <div key={item.id}>
+              <TestimonialCard
+                image={item.image}
+                name={item.name}
+                job={item.job}
+                description={item.description}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- components/testimonial/TestimonialCard.tsx
+++ components/testimonial/TestimonialCard.tsx
@@ -1,8 +1,43 @@
 import React from 'react'
+
+import star from '../../assets/icons/magic-star.svg'
+
+import Image, { StaticImageData } from 'next/image'
+
 import styles from './TestimonialCard.module.scss'
 
-const TestimonialCard = () => {
-  return <div className={styles.main}>TestimonialCard</div>
+type TestimonialCardProps = {
+  image: StaticImageData
+  name: string
+  job: string
+  description: string
+}
+
+const TestimonialCard = ({
+  image,
+  name,
+  job,
+  description,
+}: TestimonialCardProps) => {
+  return (
+    <div className={styles.main}>
+      <div className={styles.wrapper}>
+        <Image className={styles.image} src={image} alt='imageCircle' />
+
+        <div className={styles.name}>{name}</div>
+
+        <div className={styles.job}>{job}</div>
+
+        <div className={styles.rating}>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <Image key={index} src={star} alt='' />
+          ))}
+        </div>
+
+        <div className={styles.description}>{description}</div>
+      </div>
+    </div>
+  )
 }
 
 export default TestimonialCard
